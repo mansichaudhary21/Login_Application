88589f2f145241cf3cda73b8d94cce3bbd734864
mansichaudhary21 <49548815+mansichaudhary21@users.noreply.github.com>
2020-05-19 16:03:03 +0530

    update

--- Login_Service/GatewayApplication/src/test/java/com/cg/gateway/LoginTestApplication.java
+++ Login_Service/GatewayApplication/src/test/java/com/cg/gateway/LoginTestApplication.java
@@ -1,12 +1,31 @@
 package com.cg.gateway;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.cg.gateway.exceptions.UserNotFoundException;
+import com.cg.gateway.service.CredentialService;
+
+@ExtendWith(SpringExtension.class)// integrate spring test framework with junit5
 @SpringBootTest
 public class LoginTestApplication {
 
+	@Autowired
+	private CredentialService credentialService;
+	
+	
+	//When wrong Id is provided
 	@Test
-	void contextLoads() {
+	void tests_UserById()
+	{
+		Executable executable = () -> credentialService.findById("111");
+		Assertions.assertThrows(UserNotFoundException.class, executable);
+		
 	}
 }
